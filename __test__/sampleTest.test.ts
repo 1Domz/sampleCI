0d570e8f778c84fae08a2d56a7641633f7fdf5c7
1Domz <dominiquesimbillo03@gmail.com>
2024-04-06 10:25:38 +0800

    test4

--- __test__/sampleTest.test.ts
+++ __test__/sampleTest.test.ts
@@ -31,3 +31,9 @@ test("object assignment", () => {
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
+
+test("object7 assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
