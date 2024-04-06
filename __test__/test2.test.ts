2a55145a4fcbb707717a19d1a7061010f7964aef
1Domz <dominiquesimbillo03@gmail.com>
2024-04-06 08:15:38 +0800

    added 3 test

--- __test__/test2.test.ts
+++ __test__/test2.test.ts
@@ -1,6 +1,16 @@
 test("object assignment", () => {
-    const data = { one: 1 };
-    data["two"] = 2;
-    expect(data).toEqual({ one: 1, two: 2 });
-  });
-  
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
