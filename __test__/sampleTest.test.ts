b13d9312329f731acf0b76ecea6920495d3f6c69
1Domz <dominiquesimbillo03@gmail.com>
2024-04-06 10:10:06 +0800

    test 2

--- __test__/sampleTest.test.ts
+++ __test__/sampleTest.test.ts
@@ -1,26 +1,16 @@
-test("object assignment", () => {
+test("object4 assignment", () => {
   const data = { one: 1 };
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
 
-test("object assignment", () => {
-  const data = { one: 1 };
-  data["two"] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
-});
-test("object assignment", () => {
+test("object5 assignment", () => {
   const data = { one: 1 };
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
 
-test("object assignment", () => {
-  const data = { one: 1 };
-  data["two"] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
-});
-test("object assignment", () => {
+test("object6 assignment", () => {
   const data = { one: 1 };
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
